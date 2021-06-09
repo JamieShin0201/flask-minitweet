ab3153caa2f461f3dd1aaa2b6efa8b2fbd735ddc
JamieShin0201 <jamieshin1450@gmail.com>
2021-06-09 23:35:55 +0900

    tweet, timeline 기능 구현

--- app.py
+++ app.py
@@ -60,3 +60,50 @@ def sign_up():
     } if row else None
 
     return jsonify(created_user)
+
+
+@app.route('/tweet', methods=['POST'])
+def tweet():
+    user_tweet = request.json
+    tweet = user_tweet['tweet']
+
+    if len(tweet) > 300:
+        return '300자를 초과하였습니다', 400
+
+    app.database.execute(text("""
+        INSERT INTO tweets(
+            user_id,
+            tweet
+        ) VALUES (
+            :id,
+            :tweet
+        )
+    """), user_tweet)
+
+    return '', 200
+
+
+@app.route('/timeline/<int:user_id>', methods=['GET'])
+def timeline(user_id):
+    rows = app.database.execute(text("""
+        SELECT
+            t.user_id,
+            t.tweet
+        FROM
+            tweets t
+        LEFT JOIN users_follow_list ufl ON ufl.user_id = :user_id
+        WHERE t.user_id = :user_id
+        OR t.user_id = ufl.follow_user_id
+    """), {
+        'user_id': user_id
+    }).fetchall()
+
+    timeline = [{
+        'user_id': row['user_id'],
+        'tweet': row['tweet']
+    } for row in rows]
+
+    return jsonify({
+        'user_id': user_id,
+        'timeline': timeline
+    })
