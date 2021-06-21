d1a2302c507211279890114b1e77398fc428d9a1
JamieShin0201 <jamieshin1450@gmail.com>
2021-06-21 23:44:40 +0900

    JWT, Bcrypt, decortor를 이용한 인증 및 인가 구현

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-from flask import Flask, request, jsonify, current_app
+from datetime import datetime, timedelta
+from functools import wraps
+
+import bcrypt
+import jwt
+from flask import Flask, request, jsonify, current_app, Response, g
 from flask.json import JSONEncoder
 from sqlalchemy import create_engine, text
 
@@ -100,6 +105,45 @@ def get_timeline(user_id):
     } for tweet in timeline]
 
 
+def get_user_id_and_password(email):
+    row = current_app.database.execute(text("""
+                SELECT
+                    id,
+                    hashed_password
+                FROM users
+                WHERE email = :email
+            """), {'email': email}).fetchone()
+
+    return {
+        'id': row['id'],
+        'hashed_password': row['hashed_password']
+    } if row else None
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        access_token = request.headers.get('Authorization')
+        if access_token is not None:
+            try:
+                payload = jwt.decode(access_token, current_app.config['JWT_SECRET_KEY'], 'HS256')
+            except jwt.InvalidTokenError:
+                payload = None
+
+            if payload is None:
+                return Response(status=401)
+
+            user_id = payload['user_id']
+            g.user_id = user_id
+            g.user = get_user(user_id) if user_id else None
+        else:
+            return Response(status=401)
+
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+
 def create_app(test_config=None):
     app = Flask(__name__)
 
@@ -120,14 +164,44 @@ def create_app(test_config=None):
     @app.route('/sign-up', methods=['POST'])
     def sign_up():
         new_user = request.json
+        new_user['password'] = bcrypt.hashpw(
+            new_user['password'].encode('UTF-8'),
+            bcrypt.gensalt()
+        )
+
         new_user_id = insert_user(new_user)
         created_user = get_user(new_user_id)
 
         return jsonify(created_user)
 
+    @app.route('/login', methods=['POST'])
+    def login():
+        credential = request.json
+        email = credential['email']
+        password = credential['password']
+        user_credential = get_user_id_and_password(email)
+
+        if user_credential and bcrypt.checkpw(password.encode('UTF-8'),
+                                              user_credential['hashed_password'].encode('UTF-8')):
+            user_id = user_credential['id']
+            payload = {
+                'user_id': user_id,
+                'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)
+            }
+
+            token = jwt.encode(payload, app.config['JWT_SECRET_KEY'], 'HS256')
+
+            return jsonify({
+                'access_token': token
+            })
+        else:
+            return '', 401
+
     @app.route('/tweet', methods=['POST'])
+    @login_required
     def tweet():
         user_tweet = request.json
+        user_tweet['id'] = g.user_id
         tweet = user_tweet['tweet']
 
         if len(tweet) > 300:
@@ -138,20 +212,27 @@ def create_app(test_config=None):
         return '', 200
 
     @app.route('/follow', methods=['POST'])
+    @login_required
     def follow():
         payload = request.json
+        payload['id'] = g.user_id
+
         insert_follow(payload)
 
         return '', 200
 
     @app.route('/unfollow', methods=['POST'])
+    @login_required
     def unfollow():
         payload = request.json
+        payload['id'] = g.user_id
+        
         insert_unfollow(payload)
 
         return '', 200
 
     @app.route('/timeline/<int:user_id>', methods=['GET'])
+    @login_required
     def timeline(user_id):
         return jsonify({
             'user_id': user_id,
